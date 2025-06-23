cbf3e792921b56af3a41e0c79c253fa5759bd1eb
May Nyein Chan <110740526+Leo-the-Limitless@users.noreply.github.com>
2025-06-23 17:46:17 +0000

    Cell 1.0

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "May Nyein"
+last_name = "Chan"
+
+print(first_name + " " + last_name)
---
+++ cell01/ex02/age.py
@@ -0,0 +1,6 @@
+current_age = 21
+to_be_added = 42
+
+my_age = current_age + to_be_added
+
+print(my_age)
