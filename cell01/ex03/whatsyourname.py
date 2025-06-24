87fe28e43922fa9d22e4fcaedf57b22227a930b3
May Nyein Chan <110740526+Leo-the-Limitless@users.noreply.github.com>
2025-06-24 05:46:32 +0000

    Up to 3.0 Done

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,4 @@
+first_name = input("Hey, What's your First Name? : ")
+last_name = input("and your Last Name? : ")
+
+print("Well, pleased to meet you, " + first_name + " " + last_name)
---
+++ cell02/ex02/password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+password = "Python is awesome"
+
+entered_pw = input("")
+
+if password == entered_pw:
+    print("ACCESS GRANTED.")
+else:
+    print("ACCESS DENIED.")
