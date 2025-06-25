e461fa95d9c526e824e7e1ec3cc6dd126f61fae0
May Nyein Chan <110740526+Leo-the-Limitless@users.noreply.github.com>
2025-06-25 05:14:02 +0000

    Modify 5.8

--- cell05/ex08/aff_rev_params.py
+++ cell05/ex08/aff_rev_params.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 import sys
 
-length = len(sys.argv)
-if length < 3:
+args = sys.argv[1:]
+if len(args) < 2:
     print("none")
 else:
-    i = length - 1
-    while i > 0:
-        print(sys.argv[i])
-        i -= 1
+    for rev in reversed(args):
+        print(rev)
