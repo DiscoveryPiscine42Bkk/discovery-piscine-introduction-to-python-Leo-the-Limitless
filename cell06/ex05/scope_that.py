d6e113295311daa548b5cf92813ff5e846230fa9
May Nyein Chan <110740526+Leo-the-Limitless@users.noreply.github.com>
2025-06-25 17:56:43 +0000

    Add 6.5

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def add_one(num):
+    num = num + 1
+
+number = 3
+print(number)
+
+add_one(number)
+print(number) # Calling the method did not change the value of the original variable
